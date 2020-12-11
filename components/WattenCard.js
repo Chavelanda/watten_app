d79378dcf1b20421b39209020723d75135249ef9
Giacomo Melacini <gmelacini@unibz.it>
2020-12-11 17:55:40 +0100

    Added card playing by clicking on it and then swiping

--- components/WattenCard.js
+++ components/WattenCard.js
@@ -1,17 +1,30 @@
 import React, {useState} from "react";
-import {Card} from "react-native-elements";
-import {TouchableHighlight, View, StyleSheet, Image} from "react-native";
+import {Card, Overlay} from "react-native-elements";
+import {View, StyleSheet, Image, Pressable} from "react-native";
+import GestureRecognizer, {swipeDirections} from 'react-native-swipe-gestures';
 import {getRankAndSuit, images} from "../utils";
 
 export default function WattenCard({actionName, actionId, isValid, onCardPressed, style}) {
 
+    const [pressing, setPressing] = useState(false)
+
+    const onSwipeUp = (gestureState) => {
+        isValid ? onCardPressed(actionId) : null
+    }
+
     return (
         <View style={style}>
             <Card containerStyle={isValid ? [styles.cardContainer, validStyles[getRankAndSuit(actionId)[1]]] : styles.cardContainer} wrapperStyle={styles.cardWrapper}>
-                <TouchableHighlight disabled={!isValid} style={styles.cardWrapper} onPress={() => onCardPressed(actionId)} activeOpacity={0.3} underlayColor='white'>
+                <Pressable style={styles.cardWrapper} onPress={() => setPressing(true)}>
                     <Image style={styles.cardImage} source={images[actionId].uri}/>
-                </TouchableHighlight>
+                </Pressable>
             </Card>
+
+            <GestureRecognizer style={{flex: 1}} onSwipeUp={(state) => onSwipeUp(state)}>
+                <Overlay overlayStyle={styles.overlay} isVisible={pressing} onBackdropPress={() => setPressing(false)}>
+                    <Image source={images[actionId].uri}/>
+                 </Overlay>
+            </GestureRecognizer>
         </View>
     )
 }
@@ -36,6 +49,11 @@ const styles=StyleSheet.create({
         width: 114,
         height: 190,
         margin: -13,
+    },
+    overlay: {
+        borderRadius: 20,
+        width: 150,
+        height: 240,
     },
     laab: {
         backgroundColor: '#E3FFCD'
