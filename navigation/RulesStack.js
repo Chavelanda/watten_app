fe4daff1e2abb7ea37643700f2fdc4fa59c4ff2c
Giacomo Melacini <gmelacini@unibz.it>
2020-12-15 20:50:48 +0100

    Added custom header

--- navigation/RulesStack.js
+++ navigation/RulesStack.js
@@ -5,13 +5,18 @@ import RulesScreen from '../screens/RulesScreen'
 import DeckScreen from '../screens/DeckScreen'
 import GameDesScreen from '../screens/GameDesScreen'
 import HistoryScreen from '../screens/HistoryScreen'
+import {ImageBackground, StyleSheet} from "react-native";
+import Header from "@react-navigation/stack/src/views/Header/Header";
 
 const Stack = createStackNavigator()
 
 export default function RulesStack() {
 
   return (
-    <Stack.Navigator initialRouteName='Rules'>
+    <Stack.Navigator
+        initialRouteName='Rules'
+        screenOptions={{headerStyle: styles.header, headerBackground: ()=>(<ImageBackground style={styles.header} resizeMode='stretch' source={require('../assets/sfondi/header1.png')} />)}}
+    >
       <Stack.Screen name='Rules' component={RulesScreen} />
       <Stack.Screen name='Deck' component={DeckScreen} />
       <Stack.Screen name='GameDes' component={GameDesScreen} />
@@ -19,3 +24,9 @@ export default function RulesStack() {
     </Stack.Navigator>
   )
 }
+
+const styles=StyleSheet.create({
+  header: {
+    height: 80,
+  }
+})
